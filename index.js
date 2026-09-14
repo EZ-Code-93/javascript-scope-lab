329d3d86d58223bf6605c4fad5bdd1d1c8bd6854
EZ-Code-93 <Ezra.93@live.com>
2026-09-14 16:43:18 -0600

    mod 10 function scope flatburger restaurant lab: created two globally scoped variables one of which is an array. created function for adding new burger to burgers array. created if statement for adding new burger to burgers array. created function for updating the featuredDrink variable.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+//burger array for tracking burger changes
+const burgers = ['Hamburger', 'Cheeseburger']
+//Updatable global scoped featured drink variable
+let featuredDrink = 'Strawberry Milkshake'
+
+//function for adding a new burger to the burgers array. contains a function scoped variable newBurger.
+function addBurger (){
+    const newBurger = 'Flatburger'
+    burgers.push(newBurger)
+}
+
+//if statement for adding a new burger to the burgers array. contains a block scoped variable anotherNewBurger.
+if(true){
+    const anotherNewBurger = 'Maple Bacon Burger'
+    burgers.push(anotherNewBurger)
+}
+
+//function for updating the featuredDrink variable. function contains an existing variable that is being re-defined.
+function changeFeaturedDrink(){
+    featuredDrink = 'The JavaShake'
+}
